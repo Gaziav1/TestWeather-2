49d5fcf9be0ed66eba860eb68ef0424c960aeec8
Gaziav <gaziavishakov2211@icloud.com>
2019-10-15 23:38:45 +0300

    Initial Commit

---
+++ TestWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestWeather
+//
+//  Created by Газияв Исхаков on 15.10.2019.
+//  Copyright © 2019 Газияв Исхаков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
